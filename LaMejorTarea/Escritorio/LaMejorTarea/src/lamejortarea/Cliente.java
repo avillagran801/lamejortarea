d2228c160a44da02077ccf58fba17d9c38693fcb
Jose Cortes Pizarro <joscortes2021@udec.cl>
2022-09-27 11:43:36 -0300

    Esqueleto del codigo

---
+++ LaMejorTarea/Escritorio/LaMejorTarea/src/lamejortarea/Cliente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lamejortarea;
+
+/**
+ *
+ * @author anvillagran2021
+ */
+class Cliente{
+    private String nombre;
+    private String rut;
+    
+    public Cliente(){
+    }
+}
+
+class Direccion{
+    private String direccion;
+    
+    public Direccion(){
+    }
+}
