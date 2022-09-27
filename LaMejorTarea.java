5512cf23390003cf531a747a74dba22c7a3a7e03
Ana Villagran Pardo <anvillagran2021@inf.udec.cl>
2022-09-27 10:44:14 -0300

    Hi

---
+++ LaMejorTarea.java
@@ -0,0 +1,21 @@
+package javaapplication30;
+
+public class LaMejorTarea {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+abstract class Pago{
+    private float monto;
+    private Date fecha;
+    
+    public Pago(){
+    }
+}
+
+class Efectivo extends Pago{
+    
+}
