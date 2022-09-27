0194bc0580a82f6d1cb86c82c88f4fe8547b9c92
Jose Cortes Pizarro <joscortes2021@udec.cl>
2022-09-27 10:56:53 -0300

    Net Porotos

---
+++ LaMejorTarea/src/lamejortarea/LaMejorTarea.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lamejortarea;
+
+/**
+ *
+ * @author joscortes2021
+ */
+public class LaMejorTarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
